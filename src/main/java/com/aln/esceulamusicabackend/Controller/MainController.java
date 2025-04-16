2987be63e2178fd2c37871e012d954ca8645bbef
Allan Robinson Parkinson <allanrp0110@gmail.com>
2025-04-16 07:47:00 -0600

    fix this

--- src/main/java/com/aln/esceulamusicabackend/Controller/MainController.java
+++ src/main/java/com/aln/esceulamusicabackend/Controller/MainController.java
@@ -1,17 +1,18 @@
 package com.aln.esceulamusicabackend.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hi by:Apologix ";
     }
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String error() {
         return "An error occurred by:Apologix ";
     }
